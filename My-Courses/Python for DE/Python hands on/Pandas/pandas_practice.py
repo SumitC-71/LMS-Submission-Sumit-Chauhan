772d29ed4beb603788d25de32359868112a1e5bd
Sumit Chauhan <chauhansk1712@gmail.com>
2026-03-09 09:59:36 +0530

    PRAC: practice of pandas

---
+++ My-Courses/Python for DE/Python hands on/Pandas/pandas_practice.py
@@ -0,0 +1,24 @@
+'''
+pandas for 
+stock predicition
+statistics
+clean
+analytics
+
+'''
+
+
+
+
+'''
+data frames and series
+
+
+data frame = 2D 
+series is one-dimensional array (one column)
+access labels are called index
+'''
+
+
+
+import pandas as pd
